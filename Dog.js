fa770934e3e3054178d0bdd822ad81f5800e9188
minhng97 <minh.nguyentrong97@gmail.com>
2019-11-24 21:35:23 +0700

    Add package.json, chalk, sayHi() for Dog

--- Dog.js
+++ Dog.js
@@ -1,7 +1,11 @@
+var chalk = require('chalk')
 function Dog(name){
-	
+	this.name = name;
 }
 Dog.prototype.eat = function(cat) {
 	this.stomach.push(cat);
 };
+Dog.prototype.sayHi = function() {
+	console.log('Hi i am ' + chalk.red(this.name))
+	};
 module.exports = Dog;
