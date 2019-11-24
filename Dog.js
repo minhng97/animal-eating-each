124fef9d06233727362076570a3a3a020cabf217
minhng97 <minh.nguyentrong97@gmail.com>
2019-11-24 22:04:18 +0700

    Tom eat Jerry

--- Dog.js
+++ Dog.js
@@ -1,6 +1,7 @@
 var chalk = require('chalk')
 function Dog(name){
 	this.name = name;
+	this.stomach = [];
 }
 Dog.prototype.eat = function(cat) {
 	this.stomach.push(cat);
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var Dog = require('./Dog');
+var Cat = require('./Cat');
+
 
 var dog = new Dog('Tom');
 dog.sayHi();
+var cat1 = new Cat('Jerry');
+
+dog.eat(cat1);
+console.log(dog);
